ff9bd335bef009b3392ade331a309d1bd20610c8
Benshan Mei <mbs2021@163.com>
2024-11-24 23:51:38 +0800

    修复 signals.rs 中的信号处理设置，使用 sa_sigaction 替代 sa_handler 以正确处理信号

--- src/core/signals.rs
+++ src/core/signals.rs
@@ -1,4 +1,8 @@
+use std::{io::{self, ErrorKind}, mem, ptr};
+
 use libc::{sigaction, SIG_IGN, SIGTSTP, SIGSTOP, SIGKILL, SIGCHLD, SIGINT, SIGTERM};
+
+use super::DUNE_FD;
 
 pub fn setup_signals() -> io::Result<()> {
     for i in 1..32 {
@@ -6,7 +10,7 @@ pub fn setup_signals() -> io::Result<()> {
             SIGTSTP | SIGSTOP | SIGKILL | SIGCHLD | SIGINT | SIGTERM => continue,
             _ => {
                 let mut sa: sigaction = unsafe { mem::zeroed() };
-                sa.sa_handler = SIG_IGN;
+                sa.sa_sigaction = SIG_IGN;
                 if unsafe { sigaction(i, &sa, ptr::null_mut()) } == -1 {
                     unsafe { libc::close(DUNE_FD) };
                     return Err(io::Error::new(ErrorKind::Other, format!("sigaction() {}", i)));
