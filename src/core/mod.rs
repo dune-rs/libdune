3f5543e1bc7320d129810060e255ee215ae13deb
Benshan Mei <mbs2021@163.com>
2024-11-24 23:50:06 +0800

    添加调试、APIC 和 ELF 功能模块，优化模块导入结构

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,16 +1,20 @@
+#[cfg(feature = "debug")]
 mod debug;
 mod dune;
 mod entry;
 mod percpu;
+#[cfg(feature = "apic")]
 mod apic;
 mod trap;
 mod signals;
 mod idt;
 
+#[cfg(feature = "debug")]
 pub use crate::core::debug::*;
 pub use crate::core::dune::*;
 pub use crate::core::entry::*;
 pub use crate::core::percpu::*;
+#[cfg(feature = "apic")]
 pub use crate::core::apic::*;
 pub use crate::core::trap::*;
 pub use crate::core::signals::*;
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,9 +1,11 @@
 mod log;
 mod procmaps;
 mod util;
+#[cfg(feature = "elf")]
 mod elf;
 
 pub use crate::utils::log::*;
 pub use crate::utils::procmaps::*;
 pub use crate::utils::util::*;
+#[cfg(feature = "elf")]
 pub use crate::utils::elf::*;
